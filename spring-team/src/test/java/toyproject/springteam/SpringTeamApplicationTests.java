86b47fe81ca6b54798b18ed83956abf0afa14872
osj3474 <21500404@handong.edu>
2021-01-17 16:25:06 +0900

    Add spring src

---
+++ spring-team/src/test/java/toyproject/springteam/SpringTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package toyproject.springteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
