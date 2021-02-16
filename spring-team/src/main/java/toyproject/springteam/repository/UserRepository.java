1bbafca76e53d0cad541cf354a4eb095b96a9c82
mingdyuo <41130448+mingdyuo@users.noreply.github.com>
2021-02-16 18:23:48 +0900

    Product image load

--- spring-team/src/main/java/toyproject/springteam/repository/UserRepository.java
+++ spring-team/src/main/java/toyproject/springteam/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package toyproject.springteam.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import toyproject.springteam.domain.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 }
