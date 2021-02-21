61c7e9803c927878d2af4aaab23bd0b1667b5972
오상진 <21500404@handong.edu>
2021-02-21 14:49:58 +0900

    Update passing url by product_id

--- spring-team/src/main/java/toyproject/springteam/controller/BoardController.java
+++ spring-team/src/main/java/toyproject/springteam/controller/BoardController.java
@@ -1,15 +1,27 @@
 package toyproject.springteam.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import toyproject.springteam.domain.Product;
+import toyproject.springteam.service.ProductService;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
-    @GetMapping("detail")
-    public String detail(){
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("detail/{num}")
+    public String detail(@PathVariable("num") Long id, Model model){
+        Optional<Product> prod = productService.findById(id);
+        model.addAttribute("prod", prod);
         return "board/detail";
     }
 
--- spring-team/src/main/java/toyproject/springteam/repository/ProductRepository.java
+++ spring-team/src/main/java/toyproject/springteam/repository/ProductRepository.java
@@ -6,9 +6,12 @@ import org.springframework.stereotype.Repository;
 import toyproject.springteam.domain.Product;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("SELECT p FROM Product p ORDER BY p.uploadDate DESC")
     List<Product> findRecentProducts();
+
+    Optional<Product> findById(Long id);
 }
