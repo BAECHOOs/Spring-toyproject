50c001c2c0e872897afbbb502bee63248a1be6e0
오상진 <21500404@handong.edu>
2021-03-01 23:14:21 +0900

    Add meetings 0301

--- spring-team/src/main/java/toyproject/springteam/controller/BoardController.java
+++ spring-team/src/main/java/toyproject/springteam/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import toyproject.springteam.controller.dto.UserResponseDto;
 import toyproject.springteam.domain.Product;
 import toyproject.springteam.service.ProductService;
 import toyproject.springteam.service.UserService;
@@ -26,16 +27,20 @@ public class BoardController {
     @GetMapping("detail/{num}")
     public String detail(@PathVariable("num") Long prod_id, HttpServletRequest request, Model model){
         String user_id = request.getRemoteUser();
+        UserResponseDto user = null;
         if (user_id != null) {
             Long id = Long.parseLong(user_id);
+            user = userService.findById(id);
             model.addAttribute("user", userService.findById(id));
         }
         Optional<Product> prod = productService.findById(prod_id);
         model.addAttribute("prod", prod);
 
         boolean isOwner = false;
-        if(prod.get().getUser().getUserId() == Integer.parseInt(user_id)){
-            isOwner = true;
+        if(user != null) {
+            if (prod.get().getUser().getUserId() == Integer.parseInt(user_id)) {
+                isOwner = true;
+            }
         }
 
         model.addAttribute("isOwner", isOwner);
