91be2bdc028389f0f537a48c5effdd9e70afd75e
mingdyuo <41130448+mingdyuo@users.noreply.github.com>
2021-03-07 22:09:18 +0900

    ordered products

--- spring-team/src/main/java/toyproject/springteam/repository/OrderRepository.java
+++ spring-team/src/main/java/toyproject/springteam/repository/OrderRepository.java
@@ -1,9 +1,16 @@
 package toyproject.springteam.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import toyproject.springteam.domain.Order;
+import toyproject.springteam.domain.Product;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query("SELECT p FROM Order o JOIN o.product p WHERE o.user.userId=:id ORDER BY p.uploadDate DESC")
+    List<Product> findOrderedProducts(@Param("id") Long id);
 }
--- spring-team/src/main/java/toyproject/springteam/service/OrderService.java
+++ spring-team/src/main/java/toyproject/springteam/service/OrderService.java
@@ -1,10 +1,24 @@
 package toyproject.springteam.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import toyproject.springteam.controller.dto.ProductListResponseDto;
+import toyproject.springteam.repository.LikeRepository;
+import toyproject.springteam.repository.OrderRepository;
 
-//@Transactional
-//@Service
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
 public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public List<ProductListResponseDto> getOrderedProducts(Long id){
+        return orderRepository.findOrderedProducts(id).stream()
+                .map(ProductListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 
 }
